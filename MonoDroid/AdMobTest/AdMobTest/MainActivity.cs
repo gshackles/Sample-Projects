549a3d454c2c983fda81b1361ae850bd7cb592f1
Greg Shackles <gshackles@gmail.com>
2011-02-27 19:42:13 -0500

    adding AdMob sample

---
+++ MonoDroid/AdMobTest/AdMobTest/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using Android.Views;
+
+namespace AdMobTest
+{
+    [Activity(Label = "AdMobTest", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private View _adView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            AdMobHelper.RegisterEmulatorAsTestDevice();
+
+            _adView = FindViewById(Resource.Id.Ad);
+
+            FindViewById<Button>(Resource.Id.RefreshAd).Click += delegate
+            {
+                AdMobHelper.RequestFreshAd(_adView);
+            };
+        }
+    }
+}
